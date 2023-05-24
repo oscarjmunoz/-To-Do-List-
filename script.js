6eb0759d0aebc869933f12a1174b3ff3c1ce4ced
oscar <oscarmunoz03@gmail.com>
2023-05-24 10:59:14 -0500

    index

--- script.js
+++ script.js
@@ -1,36 +1,96 @@
-// este es el array que sostendra lso items de la lista de -to do-
-let todoItems= [];
+let todoItems = [];
 
-//Esta funcion creara un nuevo objeto -to do- 
-//basado en el texto que fue ingresado en la
-//entrada de texto y lo empujara hacia 
-//el array "todoItems"
-function addTodo (text) {
-    const todo ={
-        text,
-        checked: false,
-        id: Date.now(),
-    };
+function renderTodo(todo) {
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
 
-    todoItems.push(todo);
-    console.log(todoItems)
+  const list = document.querySelector('.js-todo-list');
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+  
+  if (todo.deleted) {
+    item.remove();
+    if (todoItems.length === 0) list.innerHTML = '';
+    return
+  }
+
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
+    list.append(node);
+  }
 }
 
-//seleciona el elemento form
-const form = document.querySelector('.js-form');
-//Agregar un detector de eventos de envío
-//Add a submit event listener
-form.addEventListener('submit', event => {
-    // evita que la pagina se recargue
-    event.preventDefault();
-    // seleciona el texto ingresado en el formulario
-    const form = document.querySelector('.js-form');
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
 
-    //obten el valor de la entrada y remueves el espacio en blanco
-    const text = input.value.trim();
-    if (text !== '') {
-        addTodo(text);
-        imput.value = '';
-        input.focus();
-    }
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  const todo = {
+    deleted: true,
+    ...todoItems[index]
+  };
+  todoItems = todoItems.filter(item => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+  
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('todoItems');
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach(t => {
+      renderTodo(t);
+    });
+  }
 });
